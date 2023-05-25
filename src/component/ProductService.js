b21c821ed30ecacf049592c6e287c7bccf3692e3
Luckyyamgar <mhakuyamgar@gmail.com>
2023-05-25 10:33:21 +0530

    First commit

---
+++ src/component/ProductService.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/v1/product"; 
+
+class ProductService {
+
+    saveProduct(product) {
+        return axios.post(API_URL + "/", product);
+    }
+
+    getAllProduct() {
+        return axios.get(API_URL + "/getallproduct");
+    }
+
+    getProductById(id) {
+        return axios.get(API_URL + "/" + id);
+    }
+
+    deleteProduct(id) {
+        return axios.get(API_URL + "/delete/" + id);
+    }
+
+    editProduct(product) {
+        return axios.post(API_URL + "/editProduct/" + product.id, product);
+    }
+
+}
+
+export default new ProductService;
